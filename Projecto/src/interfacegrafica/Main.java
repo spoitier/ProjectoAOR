9a055c54cf0b09cf02acef0598eedb7451b157b5
spoitier <soniapoitier@hotmail.com>
2022-12-21 16:32:49 +0000

    alteração para string nif e telefone

--- Projecto/src/interfacegrafica/Main.java
+++ Projecto/src/interfacegrafica/Main.java
@@ -1,11 +1,27 @@
 package interfacegrafica;
 
+import programa.Aor_Autocarro;
+import programa.FicheiroDeObjectos;
+
+import java.io.IOException;
+
 public class Main {
+
+    Aor_Autocarro guedes = new Aor_Autocarro();
+    public static Aor_Autocarro leficheiro() throws IOException,ClassNotFoundException{
+        FicheiroDeObjectos fdo = new FicheiroDeObjectos();
+        fdo.abreLeitura("Aor_Autocarro");
+        Aor_Autocarro guedes = (Aor_Autocarro) fdo.leObjeto();
+        fdo.fechaLeitura();
+        return guedes;
+    }
 
     public static void main(String[] args) {
 
 
         PainelFundo painelFundo = new PainelFundo();
+
+
 
 
 
